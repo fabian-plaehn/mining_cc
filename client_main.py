3803546e7d21ebf8c1bd408c7e063052a4e001a7
fabian <fabianplaehn28@gmail.com>
2024-03-17 15:27:02 +0100

    .

--- client_main.py
+++ client_main.py
@@ -1,11 +1,4 @@
-from mining_cc.client.client_class import Client
-#the_program_to_hide = win32gui.GetForegroundWindow()
-#win32gui.ShowWindow(the_program_to_hide , win32con.SW_HIDE)
-
-def client_program():
-    client = Client()
-    client.run()
-
+from mining_cc.client.flask_client import run
 
 if __name__ == '__main__':
-    client_program()  # Already an admin here.
+    run()
